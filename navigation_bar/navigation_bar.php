c1bf0448c8769dc51e25f130d1e73d2cf15efde4
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-09-11 20:57:44 -0400

    Moved navbar.html
    -changed extension to ".php" and changed name to navigation_bar.php
    -added external CSS file in the head

--- navigation_bar/navigation_bar.php
+++ navigation_bar/navigation_bar.php
@@ -1,1 +1,23 @@
+<html>
 
+<head>
+  <link rel="stylesheet" type="text/css" href="navigation_bar.css">
+</head>
+
+<body>
+
+
+
+<div class="nav">
+
+  <a href="#Sign-In">Sign-In</a>
+
+  <a href="#Sign-Up">Sign-Up</a>
+
+  <a href="#home">Home</a>
+
+</div>
+
+</body>
+
+</html>
