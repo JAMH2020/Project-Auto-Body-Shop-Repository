4f187b1c65c97f6a8f9ed8d51fb6283b485c2d8f
BigBrainTim3 <54952402+BigBrainTim3@users.noreply.github.com>
2019-09-18 14:36:10 -0400

    Update login.php

--- login/login.php
+++ login/login.php
@@ -15,26 +15,11 @@ require_once "../database/insert.php";
 $customer_email = "";
 $customer_password = "";
  
-// Processing form data when form is submitted
+//Processes inputted data; checks if everything is correct or not
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+ 
+        $sql = "SELECT id, Email, Password FROM Accounts WHERE Email = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
@@ -71,7 +56,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Close connection
     mysqli_close($link);
-}
+
 ?>
  
 <!DOCTYPE html>
