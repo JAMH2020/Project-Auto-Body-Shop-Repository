2d2653616ef9238ab0d6cd5b361ce325c020759b
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-09-22 19:56:41 -0400

    Update navigation_bar.php

--- navigation_bar/navigation_bar.php
+++ navigation_bar/navigation_bar.php
@@ -1,11 +1,21 @@
 <link rel="stylesheet" type="text/css" href="http://www.portcreditautobodyshop.tk/navigation_bar/navigation_bar_styles.css">
 
+<div class ="title">
+<h2 class="title-heading">Port Credit Auto Repair Shop </h2>
+</div> 
 
+<div class ="phone">
+<h2 class="phone-heading">(905) 278-3382 </h2> 
+</div>
+
+<div class ="address">
+<h2 class="address-heading"> 70 Mineola Rd E, Mississauga, ON L5G 2E5 </h2>
+</div>
 
 <div class="nav">
+  
 
   <a href="#">Sign-In</a>
   <a href="http://www.portcreditautobodyshop.tk/create_account/signup.php">Sign-Up</a>
   <a href="#">Home</a>
-
 </div>
