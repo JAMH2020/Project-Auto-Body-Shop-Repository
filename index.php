05ccc3943c0f7d7fc6bf52777ea28d8c4d4b0067
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-09-23 02:41:52 -0400

    Moved code from homepage.html to index.php

--- index.php
+++ index.php
@@ -8,6 +8,41 @@
   <?php
   include 'navigation_bar/navigation_bar.php';
   ?>
+    
+     <div class="bg"> 
+      <div class="colour_cover">
+        <div class="slogan_frame">
+          <span class="slogan">Welcome To The Home Page</span> 
+        </div>
+      </div>
+    </div>  
+
+
+    <div class="services">
+      <span class= "service_title">Services Offered</span> 
+
+      <div class= "C1">
+        <div class="label_cover_oil label_cover"></div> 
+        <p class="label_title Oil_label">Oil Change</p>  
+      </div> 
+
+      <div class= "C2"> 
+        <div class="label_cover_repairs label_cover"></div> 
+        <p class="label_title repair_label">Repairs</p>
+      </div> 
+
+      <div class= "C3"> 
+        <div class="label_cover_consultation label_cover"></div> 
+        <p class="label_title Consultation_label">Consultation</p>
+      </div> 
+
+    </div>
+
+ 
+    <div class="introduction">
+      <p class="intro_title"style="color:white">INTRODUCTION</p> 
+      <span class="Intro_Para" style="color:white" > Welcome to Port Credit Secondary School's Auto Shop Webpage. Port Credit Was Founded In 1920, And Its Auto Shop Is Turning 100 Years Old In 2020. It Is Our Great Pleasure To Fix Your Car Since It is Not Only A Benifit For Our Beloved Customers, But As Well As The Children Who Are Seaking To Learn All About Fixing Cars. Our Auto Shop Is Equipped With The Most Up-Todate Equipment. The Students Are Trained By A Teacher Who Is Experienced Within The Auto Field. We Garentee That You Will Not Be Disapointed.</span> 
+    </div>
   
   
   
