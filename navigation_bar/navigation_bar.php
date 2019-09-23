ae37f62f911ec85948cb8631c3c14b2e7e0fd402
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-09-22 21:17:18 -0400

    Fixed positioning of text and links
    -fixed position of the title
    -allowed the navigation bar buttons to take up the whole page

--- navigation_bar/navigation_bar.php
+++ navigation_bar/navigation_bar.php
@@ -5,17 +5,16 @@
 </div> 
 
 <div class ="phone">
-<h2 class="phone-heading">(905) 278-3382 </h2> 
+<span class="phone-heading">(905) 278-3382 </span> 
 </div>
 
 <div class ="address">
 <h2 class="address-heading"> 70 Mineola Rd E, Mississauga, ON L5G 2E5 </h2>
 </div>
 
-<div class="nav">
-  
 
-  <a href="#">Sign-In</a>
-  <a href="http://www.portcreditautobodyshop.tk/create_account/signup.php">Sign-Up</a>
-  <a href="#">Home</a>
+<div class="nav">
+  <a class= "nav_link "href="#">Sign-In</a>
+  <a class = "nav_link" href="http://www.portcreditautobodyshop.tk/create_account/signup.php">Sign-Up</a>
+  <a  class= "left_link" href="#">Home</a>
 </div>
