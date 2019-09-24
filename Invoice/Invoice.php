88dbc8beea63a71f5be7ce7ef706effd7638321e
M0hamm3d 8a19 <54952402+BigBrainTim3@users.noreply.github.com>
2019-09-24 14:36:53 -0400

    Update Invoice.php

--- Invoice/Invoice.php
+++ Invoice/Invoice.php
@@ -1,13 +1,45 @@
 <?php
+
+//ALL CUSTOMER RELATED INFORMATION
+//customer work order number
+$work_order_number = $_SESSION['work_order_number'] = "";
+
 //customer full name
 $customer_full_name = $_SESSION['customer_full_name'] = "";
 
 //customer phone number
 $customer_phone_number = $_SESSION['customer_phone_number'] = "";
 
+//customer address 
+$customer_address = $_SESSION['customer_address'] = "";
 
+//customer email
+$customer_email = $_SESSION['customer_email'] = "";
 
+//customer invoice number
+$invoice_number = $_SESSION['invoice_number'] = "";
 
+//ALL CAR RELATED INFORMATION
+//car model year
+$car_model_year = $_SESSION['car_model_year'] = "";
+
+//car year make
+$car_make = $_SESSION['car_make'] = "";
+
+//car model
+$car_model = $_SESSION['car_model'] = "";
+
+//car VIN number
+$VIN = $_SESSION['VIN'] = "";
+
+//car license plate
+$car_license_plate = $_SESSION['car_license_plate'] = "";
+
+//intake odometer reading
+$intake_odometer_reading = $_SESSION['intake_odometer_reading'] = "";
+
+//odometer reading on return
+$return_odometer_reading = $_SESSION['return_odometer_reading'] = "";
 
 
 
