f4d9c24add7e5f9a4089606510d8b34c77978169
M0hamm3d 8a19 <54952402+BigBrainTim3@users.noreply.github.com>
2019-09-24 14:08:01 -0400

    Update Invoice.php

--- Invoice/Invoice.php
+++ Invoice/Invoice.php
@@ -1,4 +1,9 @@
 <?php
+//customer full name
+$customer_full_name = $_SESSION['customer_full_name'] = "";
+
+//customer phone number
+$customer_phone_number = $_SESSION['customer_phone_number'] = "";
 
 
 
