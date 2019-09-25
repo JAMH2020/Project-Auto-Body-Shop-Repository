c124002ec3da695f798a91231d3d4e1df7dcf47c
M0hamm3d 8a19 <54952402+BigBrainTim3@users.noreply.github.com>
2019-09-24 20:51:04 -0400

    Update insert_invoice.php

--- Invoice/insert_invoice.php
+++ Invoice/insert_invoice.php
@@ -1,23 +1,84 @@
 <?php
-/*************************************************************
-** Insert user data from the signup page into the database  **
-**************************************************************/
-//connect to the database
+
+//connects to the database 
 include '../database/connectdb.php';
-//include file to check for errors in sql statements
+
+//checks for errors in sql statements
 include '../database/error_check.php';
-//prepare sql statement to bind
+
+//connects php statements to mysql table
 $stmt_signup = $conn->prepare("INSERT INTO Customer_Accounts (First_Name, Last_Name, Password, Email) VALUES (?, ?, ?, ?)");
 $stmt_signup->bind_param("ssss", $customer_firstname, $customer_lastname, $customer_password, $customer_email);
   
-//customer's firstname
-$customer_firstname = $_POST['customer_firstname'];
-//customer's lastname
-$customer_lastname = $_POST['customer_lastname'];
-//customer's password
-$customer_password = $_POST['customer_password'];
-//customer's email
+//customer work order number
+$work_order_number = $_POST['work_order_number'];
+
+//customer full name
+$customer_full_name = $_POST['customer_full_name'];
+
+//customer phone number
+$customer_phone_number = $_POST['customer_phone_number'];
+
+//customer address 
+$customer_address = $_POST['customer_address'];
+
+//customer email
 $customer_email = $_POST['customer_email'];
+
+//customer invoice number
+$invoice_number = $_POST['invoice_number'];
+
+//car model year
+$car_model_year = $_POST['car_model_year'];
+
+//car year make
+$car_make = $_POST['car_make'];
+
+//car model
+$car_model = $_POST['car_model'];
+
+//car VIN number
+$VIN = $_POST['VIN'];
+
+//car license plate
+$car_license_plate = $_POST['car_license_plate'];
+
+//intake odometer reading
+$intake_odometer_reading = $_POST['intake_odometer_reading'];
+
+//odometer reading on return
+$return_odometer_reading = $_POST['return_odometer_reading'];
+
+//date of return vehicle
+$return_date = $_POST['return_date'];
+
+//parts removed returned or not
+$removed_part_returned = $_POST['removed_part_returned'];
+
+//costs of parts
+$parts_costs = $_POST['parts_costs'];
+
+//cost of labour
+$labour_costs = $_POST['labour_costs'];
+
+//shop supplies cost
+$supplies_cost = $_POST['supplies_costs'];
+
+//recycling and/or disposal fees
+$redi_fees = $_POST['redi_fees'];
+
+//estimated costs of everything dones
+$estimated_costs = $_POST['estimated_costs'];
+
+//Total cost of EVERYTHING
+$total_cost = $_POST['total_cost'];
+
+//Teachers name
+$teachers_name = $_POST['teachers_name'];
+
+//siganture of owner
+$owner_signature = $_POST['owner_signature'];
+
 //execute the insertion for the prepared sql statement
 insertData($stmt_signup);
 $stmt_signup->close();
