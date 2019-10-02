ee5d1b87a804b4cc658f1a1e5533c1516f94bc83
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-10-01 22:27:40 -0400

    Update footer.php

--- footer/footer.php
+++ footer/footer.php
@@ -1,1 +1,44 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="????????">
+</head>
+<body>
+  <div class="footer">
+
+    <div class="footer_items">
+
+      <div class="about">
+      <div class="memeber">
+        <img alt="" class="photo first" src="png"/>
+        <p class="name">Momhammed Baig</p>
+      </div>
+      
+      <div class="memeber">
+        <img alt="" class="photo second" src="png"/>
+        <p class="name">Alex Au</p>
+      </div>
+        
+       <div class="memeber">
+         <img alt="" class="photo third" src="png"/>
+         <p class="name">Harman Sekhon</p>
+      </div>
+
+      <div class="memeber">
+        <img alt="" class="photo first" src="png"/>
+        <p class="name">Joshua Noronha</p>
+      </div>
+      </div>
+      
+    
+
+    </div>
+
+  </div>
+<div class="conclusion">
+  <span class="conclusion_info">That's all folks!</span>
+</div>
+</body>
+</html>
 
