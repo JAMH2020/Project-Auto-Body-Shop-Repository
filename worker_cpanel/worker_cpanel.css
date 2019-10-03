edc40ccf65d41e69d0a579473e7154daa33b2beb
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-10-03 13:20:21 -0400

    Create worker_cpanel.css

---
+++ worker_cpanel/worker_cpanel.css
@@ -0,0 +1,23 @@
+.body {
+  margin: 0; 
+  background-color:	white;
+}
+
+.title{
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif; 
+  position: relative;
+  font-size: 30px;
+  left: 50px; 
+  color: rgb(3, 55, 107); 
+  text-shadow: 1px 2px 2px #66c2ff;
+  font-weight: bold;
+  margin: 10px 0 0 0;
+  
+}
+
+.order{
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  position: relative;
+  font-size: 20px;
+  left: 50px; 
+}
