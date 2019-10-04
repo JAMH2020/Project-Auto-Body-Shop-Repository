569d08fac6fd4a93afae2310ef113390dc52baf2
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-04 04:33:44 -0400

    Allow page to create invoices
    -each order will have a unique link to save its unique values into session variables to used to make an invoice

--- worker_cpanel/worker_cpanel.php
+++ worker_cpanel/worker_cpanel.php
@@ -10,6 +10,14 @@ if (session_start() === null){
 <!DOCTYPE html>
 <html>
 <head>
+  <!--JQuery library-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+  <!--script use to redirect the user to another page-->
+  <script src="../src/js/submit_form.js"></script>
+  
+  <!--script for finding the value of a certain row in the customer table without the refresh of the page-->
+  <script src="../database/findRow.js"></script>
 </head>
 
 <body>
@@ -18,7 +26,8 @@ if (session_start() === null){
 include "../navigation_bar/navigation_bar.php";
 ?>
 
-<a href="intake_repair_form.php">Create Order</a>
+<a href="orders/intake_repair_form.php">Create Order</a>
+<a href="invoices/invoice.php">Create Invoice</a>
 
 <h2>My Orders</h2>
 
@@ -30,3 +39,4 @@ include '../database/select/select_orders.php';
 
 
 </body>
+</html>
