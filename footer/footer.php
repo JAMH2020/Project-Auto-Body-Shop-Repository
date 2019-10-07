f8728ae759cba95d8b9d6bf6eae644fbd60bb70d
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-07 05:40:41 -0400

    Centered the images

--- footer/footer.php
+++ footer/footer.php
@@ -1,41 +1,37 @@
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="????????">
-</head>
-<body>
-  <div class="footer">
-    <div class="footer_items">
+<!--footer of the page-->
+<link rel="stylesheet" type="text/css" href="http://www.portcreditautobodyshop.tk/footer/footer_styles.css">
+  
+<div class="footer">
 
-      <div class="about">
-      <div class="memeber">
-        <img alt="" class="photo first" src="Momhammed.jpeg.jpg"/>
-        <p class="name">Momhammed Baig</p>
+  <div class="footer_items">
+    
+      <div class="member">
+         <img alt="" class="photo first" src="http://www.portcreditautobodyshop.tk/images/footer/Baig.jpg"/> 
+         <p class="name">Momhammed Baig<p>
       </div>
 
-      <div class="memeber">
-        <img alt="" class="photo second" src="Alex.jpeg.jpg"/>
+
+      <div class="member">
+        <img alt="" class="photo second" src="http://www.portcreditautobodyshop.tk/images/footer/Alex.png"/>
         <p class="name">Alex Au</p>
       </div>
 
-       <div class="memeber">
-         <img alt="" class="photo third" src="Harman.jpeg.jpg"/>
-         <p class="name">Harman Sekhon</p>
+
+      <div class="member">
+        <img alt="" class="photo third" src="http://www.portcreditautobodyshop.tk/images/footer/Harman.png"/>
+        <p class="name">Harman Sekhon</p>
       </div>
 
-      <div class="memeber">
-        <img alt="" class="photo first" src="Josh.jpeg.jpg"/>
+
+      <div class="member">
+        <img alt="" class="photo first" src="http://www.portcreditautobodyshop.tk/images/footer/Josh.png"/>
         <p class="name">Joshua Noronha</p>
       </div>
-      </div>
-
-
-    </div>
-
+   
   </div>
+</div>
+
 <div class="conclusion">
   <span class="conclusion_info">That's all folks!</span>
 </div>
-</body>
-</html>
+
