29663223a8abcf247c702845aa469b8b238d55df
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-07 05:33:38 -0400

    Update worker_cpanel.php

--- worker_cpanel/worker_cpanel.php
+++ worker_cpanel/worker_cpanel.php
@@ -6,35 +6,49 @@ if (session_start() === null){
 ?>
 
 <!-- control panel for the worker -->
-
 <!DOCTYPE html>
 <html>
 <head>
+  <!--style sheet for the worker control panel-->
+  <link rel="stylesheet" type="text/css" href="worker_cpanel_styles.css">
+  
+  <!--style sheet for table of the worker control panel-->
+  <link rel="stylesheet" type="text/css" href="../database/select/css/select_orders.css">
+
+
   <!--JQuery library-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
   <!--script use to redirect the user to another page-->
   <script src="../src/js/submit_form.js"></script>
-
+  
   <!--script for finding the value of a certain row in the customer table without the refresh of the page-->
   <script src="../database/findRow.js"></script>
+  
 </head>
 
 <body>
+
 <?php
 //include the navigation bar
 include "../navigation_bar/navigation_bar.php";
 ?>
 
+
+
+<h2 class='order_title'>MY ORDERS</h2>
+
 <a href="intake_repair_form.php" class='order'>Create Order</a>
-
-<h2 class='title'>MY ORDERS</h2>
-
+<div class="create_order_underline"></div>
 
 <?php
 //include file for selecting orders
 include '../database/select/select_orders.php';
 ?>
+
+
+</body>
+</html>
 
 
 
