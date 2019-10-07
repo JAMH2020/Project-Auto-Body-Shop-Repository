b7d13f956ee329a30e517b52158212d09575cb94
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-07 05:44:31 -0400

    Update select_orders.php

--- database/select/select_orders.php
+++ database/select/select_orders.php
@@ -2,37 +2,50 @@
 /********************************************************
 ** shows all the orders of the worker who is logged on **
 *********************************************************/
+
 //include file to connect to the database
 include_once '../database/connectdb.php';
+
+
 
 //include file to check errors in sql statements
 include_once '../database/error_check.php';
 
+
+
 //prepare and bind sql statement
-//first part of the orders table
 //inner join the customer accounts table into the orders table in order to get the customer's name using the customer id as the key
 $stmt_orders1 = $conn->prepare("SELECT Orders.Order_Id, Orders.Order_No, Orders.Date, Customer_Accounts.First_Name, Customer_Accounts.Last_Name, Orders.Description, Orders.Work_Date, Orders.Odometer_Intake, Orders.School_Name, Orders.School_Address, Orders.Status
                                 FROM Orders
                                 INNER JOIN Customer_Accounts ON Orders.Customer_Id=Customer_Accounts.Customer_Id
                                 WHERE Orders.Worker_Id = ?");
+
 $stmt_orders1->bind_param("i", $worker_id);
+
 //worker id from the sessions in the login
 $worker_id = 1;
+
 //execute the prepared statement
 $stmt_orders1->execute();
+
 //store the selected result
 $stmt_orders1->store_result();
+
 //bind the selected results
 $stmt_orders1->bind_result($order_idRow, $order_noRow, $dateRow, $customer_firstnameRow, $customer_lastnameRow, $plan_descriptionRow, $plan_dateRow, $odometer_intakeRow, $school_nameRow, $school_addressRow, $statusRow);
 
 
 
+
+
 //prints out the orders if there are any
 if ($stmt_orders1->num_rows > 0){
+
   //print table out
   echo "<table class='table'>";
-    echo "<tr>";
 
+    echo "<tr>";
+    
       echo "<th>Order Number</th>";
       echo "<th>Date</th>";
       echo "<th>Customer Name</th>";
@@ -43,7 +56,7 @@ if ($stmt_orders1->num_rows > 0){
       echo "<th>School Address</th>";
       echo "<th>Status</th>";
       echo "<th></th>";
-
+      
     echo "</tr>";
 
   //print out the details of each order
@@ -62,14 +75,12 @@ if ($stmt_orders1->num_rows > 0){
            <td>
              <a href='#' onclick='findCAccountRow("<?php echo $order_idRow?>", "../database/select/find_row/find_row_orders.php", "invoices/invoice.php"); return false;'>Create Invoice</a>
            </td>
-           
-<?php
 
+<?php
    echo "</tr>";
   }
-
+  
   echo "</table>";
-
   echo "<div id='rowText'></div>";
 
 //if there are no orders for the specific id
@@ -77,6 +88,7 @@ if ($stmt_orders1->num_rows > 0){
   echo "<h3 class='conclusion'>" . "There Are No Orders Available" . "</h3>";
   exit();
 }
+
 //close the statement
 $stmt_orders1->close();
 
