c58bcfeb783a6f1d5f48d42a36e77d204ef5ba5e
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-10-09 12:45:03 -0400

    Update aselect_caccounts.php

--- database/select/aselect_caccounts.php
+++ database/select/aselect_caccounts.php
@@ -2,34 +2,24 @@
 /***********************************************
 ** shows all the customer accounts available  **
 ************************************************/
-
-
 //include file to connect to the database
 include_once '../../database/connectdb.php';
-
 //include file to check errors in sql statements
 include_once '../../database/error_check.php';
-
-
 //prepare and bind sql statement
 $stmt_a_caccounts = $conn->prepare("SELECT * FROM Customer_Accounts ORDER BY Last_Name");
-
 //execute the statement
 $stmt_a_caccounts->execute();
-
 //store the result
 $stmt_a_caccounts->store_result();
-
 //bind the results
 $stmt_a_caccounts->bind_result($customer_idRow, $customer_firstnameRow, $customer_lastnameRow, $customer_passwordRow, $customer_emailRow);
-
 //$path = "../../database/select/find_row/find_row_caccounts.php";
-
 //print out the accounts that are available
 if ($stmt_a_caccounts->num_rows > 0){
-
   //prints out a table
-  echo "<table>";
+
+  echo "<table class='table'>";
     echo "<tr>";
       
       echo "<th>Customer Id</th>";
@@ -39,7 +29,6 @@ if ($stmt_a_caccounts->num_rows > 0){
       echo "<th></th>";
       
     echo "</tr>";
-
   while($stmt_a_caccounts->fetch()){
    echo "<tr>";
       echo "<td> <input type='checkbox' name='customer_idArr[]' value=" . $customer_idRow .">" . $customer_idRow . "</td>";
@@ -59,11 +48,8 @@ if ($stmt_a_caccounts->num_rows > 0){
    echo "</table>";
    
 } else {
-  echo "<h3>" . "There are no accounts available" . "</h3>";
+  echo "<h3 class='conclusion'>" . "There Are No Accounts Available" . "</h3>";
 }
-
-
-
 //close the statement
 $stmt_a_caccounts->close();
 ?>
