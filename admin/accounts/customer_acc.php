fad15db290b669421d448861b5ea295f0b2af55a
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-10-09 12:30:22 -0400

    Update customer_acc.php

--- admin/accounts/customer_acc.php
+++ admin/accounts/customer_acc.php
@@ -5,22 +5,32 @@ if (session_start() === null){
 }
 ?>
 
-<!-- control panel for the worker -->
-
-<!DOCTYPE html>
+<!--the admin control panel section where they can check the customer accounts in the system-->
 <html>
 <head>
+  <!--JQuery library-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+  <!--script use to redirect the user to another page-->
+  <script src="../../src/js/submit_form.js"></script>
+  
+  <!--script for finding the value of a certain row in the customer table without the refresh of the page-->
+  <script src="../../database/findRow.js"></script>
 </head>
-
 <body>
-<?php
-//include the navigation bar
-include "../navigation_bar/navigation_bar.php";
-?>
+  <?php
+  //include the navigation bar
+  include_once "../../navigation_bar/navigation_bar.php";
+  //include the file that will print out all the orders
+  include "../../database/select/aselect_caccounts.php";
+  ?>
+  
+  <a href="../admin_cpanel.php" class='back'>Back</a>
+  
+  <div id="rowText"></div>
+  
 
-<a href="intake_repair_form.php" class='order'>Create Order</a>
-
-<h2 class='title'>MY ORDERS</h2>
-
-</body
+  
+</body>
 </html>
+
