a10791065ba9cfc5bd1a5203e87c67e722bfdd00
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-10 05:07:30 -0400

    Update and rename temp2.php to check_invoices.php

---
+++ admin/invoices/check_invoices.php
@@ -0,0 +1,38 @@
+<?php
+//start the session if it has not been started yet
+if (session_start() === null){
+  session_start();
+}
+?>
+
+
+<!--the admin control panel section where they can check the customer profiles in the system-->
+<html>
+<head>
+
+   <!--JQuery library-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  
+  <!--script use to redirect the user to another page-->
+  <script src="../../src/js/submit_form.js"></script>
+  
+  <!--script for finding the value of a certain row in the customer table without the refresh of the page-->
+  <script src="../../database/findRow.js"></script>
+</head>
+<body>
+
+  <?php
+  //include the navigation bar
+  include "../../navigation_bar/navigation_bar.php";
+  
+  //include the file that will print out all the orders
+  include "../../database/select/aselect_invoices.php";
+  ?>
+  
+  <a href="../admin_cpanel.php">Back</a>
+  
+  <div id="rowText"></div>
+
+  
+</body>
+</html>
