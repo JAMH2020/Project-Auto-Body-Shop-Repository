34c6d4bee43322bf4e7a7787aa7502d5dac58824
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-10-13 19:52:15 -0400

    Added function to get customer id
    -gets customer id using customer email

--- database/select/aselect_caccounts.php
+++ database/select/aselect_caccounts.php
@@ -2,54 +2,120 @@
 /***********************************************
 ** shows all the customer accounts available  **
 ************************************************/
-//include file to connect to the database
-include_once '../../database/connectdb.php';
-//include file to check errors in sql statements
-include_once '../../database/error_check.php';
-//prepare and bind sql statement
-$stmt_a_caccounts = $conn->prepare("SELECT * FROM Customer_Accounts ORDER BY Last_Name");
-//execute the statement
-$stmt_a_caccounts->execute();
-//store the result
-$stmt_a_caccounts->store_result();
-//bind the results
-$stmt_a_caccounts->bind_result($customer_idRow, $customer_firstnameRow, $customer_lastnameRow, $customer_passwordRow, $customer_emailRow);
-//$path = "../../database/select/find_row/find_row_caccounts.php";
-//print out the accounts that are available
-if ($stmt_a_caccounts->num_rows > 0){
-  //prints out a table
 
-  echo "<table class='table'>";
-    echo "<tr>";
+//list the customer's accounts
+function list_caccounts(){
+  //include file to connect to the database
+  include_once '../../database/connectdb.php';
+
+  //include file to check errors in sql statements
+  include_once '../../database/error_check.php';
+
+
+  //prepare and bind sql statement
+  $stmt_a_caccounts = $conn->prepare("SELECT * FROM Customer_Accounts ORDER BY Last_Name");
+
+  //execute the statement
+  $stmt_a_caccounts->execute();
+
+  //store the result
+  $stmt_a_caccounts->store_result();
+
+  //bind the results
+  $stmt_a_caccounts->bind_result($customer_idRow, $customer_firstnameRow, $customer_lastnameRow, $customer_passwordRow, $customer_emailRow);
+
+  //print out the accounts that are available
+  if ($stmt_a_caccounts->num_rows > 0){
+
+    //prints out a table
+    echo "<table class='table'>";
+      echo "<tr>";
       
-      echo "<th>Customer Id</th>";
-      echo "<th>Name</th>";
-      echo "<th>Password</th>";
-      echo "<th>Email</th>";
-      echo "<th></th>";
+        echo "<th>Customer Id</th>";
+        echo "<th>Name</th>";
+        echo "<th>Password</th>";
+        echo "<th>Email</th>";
+        echo "<th></th>";
       
-    echo "</tr>";
-  while($stmt_a_caccounts->fetch()){
-   echo "<tr>";
-      echo "<td> <input type='checkbox' name='customer_idArr[]' value=" . $customer_idRow .">" . $customer_idRow . "</td>";
-      echo "<td>" . $customer_firstnameRow . " " . $customer_lastnameRow . "</td>";
-      echo "<td>" . $customer_passwordRow . "</td>";
-      echo "<td>" . $customer_emailRow . "</td>";
+      echo "</tr>";
+
+    while($stmt_a_caccounts->fetch()){
+     echo "<tr>";
+        echo "<td> <input type='checkbox' name='customer_idArr[]' value=" . $customer_idRow .">" . $customer_idRow . "</td>";
+        echo "<td>" . $customer_firstnameRow . " " . $customer_lastnameRow . "</td>";
+        echo "<td>" . $customer_passwordRow . "</td>";
+        echo "<td>" . $customer_emailRow . "</td>";
       
 ?>
-            <td>
-              <a href='#' onclick='findCAccountRow("<?php echo $customer_idRow?>", "../../database/select/find_row/find_row_caccounts.php", "change/change_account.php"); return false;'>Edit</a>
-            </td>
+              <td>
+                <a href='#' onclick='findCAccountRow("<?php echo $customer_idRow?>", "../../database/select/find_row/find_row_caccounts.php", "change/change_account.php"); return false;'>Edit</a>
+              </td>
 <?php
      
-    echo "</tr>";
-   }
+      echo "</tr>";
+     }
    
-   echo "</table>";
+     echo "</table>";
    
-} else {
-  echo "<h3 class='conclusion'>" . "There Are No Accounts Available" . "</h3>";
+  } else {
+    echo "<h3 class='conclusion'>" . "There are no accounts available" . "</h3>";
+  }
+
+
+
+  //close the statement
+  $stmt_a_caccounts->close();
+
 }
-//close the statement
-$stmt_a_caccounts->close();
+
+
+
+
+
+
+// get the customer's id using their email
+function get_caccounts_id($conn){
+
+  //include file to connect to the database
+  include_once '../../database/connectdb.php';
+
+  //include file to check errors in sql statements
+  include_once '../../database/error_check.php';
+
+
+  //prepare and bind sql statement
+  $stmt_a_caccounts = $conn->prepare("SELECT Customer_Id FROM Customer_Accounts WHERE Email = ?");
+  $stmt_a_caccounts->bind_param("s", $customer_email);
+  
+  
+  //customer_email
+  $customer_email = $_SESSION['customer_email'];
+  
+
+  //execute the statement
+  $stmt_a_caccounts->execute();
+
+  //store the result
+  $stmt_a_caccounts->store_result();
+
+  //bind the results
+  $stmt_a_caccounts->bind_result($customer_idRow);
+
+  if ($stmt_a_caccounts->num_rows > 0){
+  
+    while($stmt_a_caccounts->fetch()){
+    
+      //get the customer id
+      $customer_id = $customer_idRow;
+      echo "customer_id: " . $customer_id;
+    
+    }
+  } 
+  
+  
+  
+  
+  //return the customer id
+  return $customer_id;
+}
 ?>
