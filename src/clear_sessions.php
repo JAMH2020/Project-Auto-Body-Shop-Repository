6bdbe1c32eda38c57a989d7516ebd45471023cf4
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-23 05:44:51 -0500

    Create clear_sessions.php
    clears any saved sessions after editing files

---
+++ src/clear_sessions.php
@@ -0,0 +1,10 @@
+<?php
+//set all session variables for order to blank
+$_SESSION['order_no'] = $_SESSION['school_name'] = $_SESSION['school_address'] = $_SESSION['car_year'] = $_SESSION['car_make'] = $_SESSION['car_model'] =
+$_SESSION['vin_no'] = $_SESSION['license_plate'] = $_SESSION['odometer_intake'] = $_SESSION['plan_description'] = $_SESSION['plan_date'] = 
+$_SESSION['estimate_parts_per_unit'] = $_SESSION['estimate_parts_total'] = $_SESSION['estimate_labour_per_unit'] = $_SESSION['estimate_labour_total'] =
+$_SESSION['estimate_supplies_per_unit'] = $_SESSION['estimate_supplies_total'] = $_SESSION['estimate_disposal_per_unit'] = $_SESSION['estimate_disposal_total'] =
+$_SESSION['estimate_total_cost'] = $_SESSION['estimate_date'] = $_SESSION['estimate_expiry_date'] = $_SESSION['removal_choice'] = $_SESSION['customer_firstname'] =
+$_SESSION['customer_lastname'] = $_SESSION['customer_address'] = $_SESSION['customer_email'] = $_SESSION['customer_phone'] = $_SESSION['waiver_date'] = 
+$_SESSION['customer_initial'] = $_SESSION['exceed_cost'] = $_SESSION['worker_id'] = "";
+?>
