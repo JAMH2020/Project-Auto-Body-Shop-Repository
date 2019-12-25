7d9b1e398ef8d03bdb44f8dacf157922922fef75
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-25 04:49:34 -0500

    Create insert_wsignup.php

---
+++ database/insert/insert_wsignup.php
@@ -0,0 +1,37 @@
+<?php
+/*************************************************************
+** Insert user data from the signup page into the database  **
+**************************************************************/
+
+
+//connect to the database
+include_once '../database/connectdb.php';
+
+//include file to check for errors in sql statements
+include_once '../database/error_check.php';
+
+//prepare sql statement to bind
+$stmt_wsignup = $conn->prepare("INSERT INTO Worker_Accounts (First_Name, Last_Name, Password, Email) VALUES (?, ?, ?, ?)");
+$stmt_wsignup->bind_param("ssss", $worker_firstname, $worker_lastname, $worker_password, $worker_email);
+  
+
+//worker's firstname
+$worker_firstname = $_POST['worker_firstname'];
+
+//worker's lastname
+$worker_lastname = $_POST['worker_lastname'];
+
+//customer's password
+$worker_password = $_POST['worker_password'];
+
+//worker's email
+$worker_email = $_POST['worker_email'];
+
+
+//execute the insertion for the prepared sql statement
+insertData($stmt_wsignup);
+
+$stmt_wsignup->close();
+
+
+?>
