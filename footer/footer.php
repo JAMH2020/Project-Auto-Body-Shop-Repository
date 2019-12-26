671e9ad8ba6d03840b85cfc5f3d32d9221428f93
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-26 18:13:24 -0500

    Added Contacts Page
    -added social media links and copyright

--- footer/footer.php
+++ footer/footer.php
@@ -1,9 +1,9 @@
 <!--footer of the page-->
 <link rel="stylesheet" type="text/css" href="http://www.portcreditautobodyshop.tk/footer/footer_styles.css">
   
-<div class="footer">
+  <div class="footer">
 
-  <div class="footer_items">   
+  <div class="footer_items">
     
       <div class="member">
          <img alt="" class="photo first" src="http://www.portcreditautobodyshop.tk/images/footer/Baig.jpg"/> 
@@ -31,6 +31,43 @@
   </div>
 </div>
 
-<div class="conclusion">
-  <span class="conclusion_info">That's all folks!</span>
+<div class="footer_contacts">
+  <div class="contacts_box">
+    <p>contacts</p>
+
+    <div class="social_media_box">
+ 
+      <a class="social_media_link" href="#" style="opacity:1">
+        <i class="material-icons">email</i>
+      </a>
+
+      <a class="social_media_link" href="#">
+        <i class='fab fa-facebook'></i>
+      </a>
+ 
+      <a class="social_media_link" href="#">
+        <i class='fab fa-twitter'></i>
+      </a>
+
+      <a class="social_media_link" href="#">
+        <i class='fab fa-instagram'></i>
+      </a>
+
+      <a class="social_media_link" href="https://github.com/JAMH2020/Project-Auto-Body-Shop-Repository">
+        <i class='fab fa-github'></i>
+      </a>
+
+        
+    </div>
+  </div>
+
+  <span class="copy_right_text">
+    <i class='far fa-copyright copy_right_symbol'></i> <?php echo " ". date("Y") . " ";?>JAMH Group
+  </span>
 </div>
+
+
+  
+</body>
+
+</html>
