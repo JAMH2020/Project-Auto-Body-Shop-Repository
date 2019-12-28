61508c812c5b3f91bb2547ba25ac2b3c9ffe9cf8
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-12-28 15:57:27 -0500

    Update navigation_bar.php

--- navigation_bar/navigation_bar.php
+++ navigation_bar/navigation_bar.php
@@ -1,9 +1,9 @@
+<link rel="stylesheet" type="text/css" href="http://www.portcreditautobodyshop.tk/navigation_bar/navigation_bar_styles.css">
 
 <link rel="stylesheet" type="text/css" href="http://www.portcreditautobodyshop.tk/navigation_bar/navigation_bar_styles.css">
 
 <!--Google Titillium font-->
 <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
-
 
 <!--JQuery library-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -18,24 +18,35 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <div class="title_bar" id="stickyTitleX">
+
       <div class ="title">
         <span class="title-heading title_front">Port Credit</span>
         <span class="title-heading title_back">Auto Repair Shop</span>
-      </div> 
+      </div>
 
       <div class="site_information">
         <div class ="phone">
-          <span class="phone-heading title_details">(905) 278-3382 </span> 
+          <span class="phone-heading title_details">(905) 278-3382</span> 
         </div>
       </div>
 
       <div class="site_information">
         <div class ="address">
-          <span class="address-heading title_details"> 70 Mineola Rd E, Mississauga, ON L5G 2E5 </span>
+          <span class="address-heading title_details"> 70 Mineola Rd E, Mississauga, ON L5G 2E5</span>
         </div>
       </div>
-    </div>
+    
+      <a class= "right_title_link" href="http://www.portcreditautobodyshop.tk">
+        Sign-Out
+        <div class="nav_link_underline"></div>
+      </a>
+      
+      <a class= "left_title_link" href="http://www.portcreditautobodyshop.tk">
+        User Profile
+        <div class="nav_link_underline"></div>
+      </a>
 
+    </div>
 
     <div class="filler_nav filler_hide">
     </div>
@@ -47,14 +58,12 @@
         Home
         <div class="nav_link_underline"></div>
       </a>
-
     
       <a class = "nav_link" href="http://www.portcreditautobodyshop.tk/create_account/signup.php">
         <div class="nav_link_underline"></div>
         Sign-Up
         <div class="nav_link_underline"></div>
       </a>
-
 
       <a class= "nav_link "href="http://www.portcreditautobodyshop.tk/login/login.php">
         <div class="nav_link_underline"></div>
@@ -63,4 +72,3 @@
       </a>
 
     </div>
-
