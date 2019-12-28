64c86ab84aac15474f251b4e57dad665c2645b8f
Harman15943 <45205344+Harman15943@users.noreply.github.com>
2019-12-28 16:37:55 -0500

    Update aselect_waccounts.php

--- database/select/aselect_waccounts.php
+++ database/select/aselect_waccounts.php
@@ -1,66 +1,51 @@
 <?php
-/******************************************
-** shows all the invoices that are done  **
-*******************************************/
+/*********************************************
+** shows all the worker accounts available  **
+**********************************************/
 //include file to connect to the database
 include_once '../../database/connectdb.php';
 //include file to check errors in sql statements
 include_once '../../database/error_check.php';
 //prepare and bind sql statement
-$stmt_ainvoice = $conn->prepare("SELECT * FROM Invoice");
+$stmt_a_waccounts = $conn->prepare("SELECT * FROM Worker_Accounts");
 //execute the statement
-$stmt_ainvoice->execute();
+$stmt_a_waccounts->execute();
 //store the result
-$stmt_ainvoice->store_result();
+$stmt_a_waccounts->store_result();
 //bind the results
-$stmt_ainvoice->bind_result($invoice_idRow, $order_noRow, $worker_idRow, $invoice_noRow, $invoice_dateRow, $odometer_returnRow, $descriptionRow, $authorization_dateRow, $completion_dateRow);
-//print out the orders that are available
-if ($stmt_ainvoice->num_rows > 0){
-//prints out a table
+$stmt_a_waccounts->bind_result($worker_idRow, $worker_firstnameRow, $worker_lastnameRow, $worker_passwordRow, $worker_emailRow);
+//print out the accounts that are available
+if ($stmt_a_waccounts->num_rows > 0){
+  //prints out a table
   echo "<table class='table'>";
     echo "<tr>";
       
-      echo "<th>Invoice Id</th>";
-      echo "<th>Invoice Number</th>";
-      echo "<th>Order Number</th>";
       echo "<th>Worker Id</th>";
-      echo "<th>Invoice Date</th>";
-      echo "<th>Odometer Return Value</th>";
-      echo "<th>Description of Work Done</th>";
-      echo "<th>Authorization Date of Work</th>";
-      echo "<th>Completion Date</th>";
-      echo "<th></th>";
+      echo "<th>Name</th>";
+      echo "<th>Password</th>";
+      echo "<th>Email</th>";
+      
     echo "</tr>";
-  
-  while($stmt_ainvoice->fetch()){
+  while($stmt_a_waccounts->fetch()){
     echo "<tr>";
-      echo "<td> <input type='checkbox' name='order_idArr[]' value=" . $invoice_idRow .">" . $invoice_idRow . "</td>";
-      echo "<td>" . $invoice_noRow . "</td>";
-      echo "<td>" . $order_noRow . "</td>";
-      echo "<td>" . $worker_idRow . "</td>";
-      echo "<td>" . $invoice_dateRow . "</td>";
-      echo "<td>" . $odometer_returnRow . "</td>";
-      echo "<td>" . $descriptionRow . "</td>";
-      echo "<td>" . $authorization_dateRow . "</td>";
-      echo "<td>" . $completion_dateRow . "</td>";
+      echo "<td> <input type='radio' name='order_id' value=" . $worker_idRow .">" . $worker_idRow . "</td>";
+      echo "<td>" . $worker_firstnameRow . " " . $worker_lastnameRow . "</td>";
+      echo "<td>" . $worker_passwordRow . "</td>";
+      echo "<td>" . $worker_emailRow . "</td>";
 ?>
 
             <td>
-              <a href='#' onclick='findCAccountRow("<?php echo $invoice_idRow?>", "../../database/select/find_row/find_row_invoices.php", "change/change_account.php"); return false;'>Edit</a>
+              <a href='#' onclick='findCAccountRow("<?php echo $worker_idRow?>", "../../database/select/find_row/find_row_waccounts.php", "change/change_account.php"); return false;'>Edit</a>
             </td>
 
-<?php
-     
+<?php     
     echo "</tr>";
   }
   
   echo "</table>";
   
-//if there are no orders
 } else {
-  echo "<h3 class='conclusion'>" . "There Are No Invoices Available" . "</h3>";
-  exit();
+  echo "<h3 class='conclusion' >" . "There Are No Accounts Available" . "</h3>";
 }
-//close the statment
-$stmt_ainvoice->close();
-?>
+//close the statement
+$stmt_a_waccounts->close();
