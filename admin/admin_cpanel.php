76698e03f8aecb1e35dec0db88a5e79b2e97f669
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-28 01:16:48 -0500

    Changed control panel to be asynchronous
    -used AJAX to go to different sections of the page instead of always redirecting to another page

--- admin/admin_cpanel.php
+++ admin/admin_cpanel.php
@@ -3,12 +3,27 @@
 if (session_start() === null){
   session_start();
 }
+
+//clear saved session variable from other pages
+include "../src/clear_sessions.php";
+
+clear_order();
+clear_invoice();
 ?>
 
 <html>
 <head>
   <!--style sheet for the worker control panel-->
   <link rel="stylesheet" type="text/css" href="css/admin_cpanel_styles.css">
+  
+  <!--script to load a certain section-->
+  <script src="../src/js/load_file.js"></script>
+  
+  <!--script use to redirect the user to another page-->
+  <script src="../src/js/submit_form.js"></script>
+  
+   <!--script for finding the value of a certain row in the customer table without the refresh of the page-->
+  <script src="../database/findRow.js"></script>
 
 </head>
 <body>
@@ -18,11 +33,48 @@ if (session_start() === null){
 ?>
 
 <div class="admin_cpanelPage">
-  <a href="orders/check_orders.php">Check Orders</a>
-  <a href="invoices/check_invoices.php">Check Invoices</a>
-  <a href="accounts/customer_acc.php">Check Customer Accounts</a>
-  <a href="profiles/check_profiles.php">Check Profiles</a>
-  <a href="accounts/worker_acc.php">Check Worker Accounts</a>
+  <a href="#" onclick="loadFile('orders/check_orders.php')">Check Orders</a>
+  <a href="#" onclick="loadFile('invoices/check_invoices.php')">Check Invoices</a>
+  <a href="#" onclick="loadFile('accounts/customer_acc.php')">Check Customer Accounts</a>
+  <a href="#" onclick="loadFile('profiles/check_profiles.php')">Check Profiles</a>
+  <a href="#" onclick="loadFile('accounts/worker_acc.php')">Check Worker Accounts</a>
+  
+  <div class="content_window">
+    <?php
+    //sessions to choose which section to load to the control panel
+    if ($_SESSION['admin_section'] == "waccounts"){
+    ?>
+    
+      <script> loadFile('accounts/worker_acc.php'); </script>
+      
+    <?php
+    } else if ($_SESSION['admin_section'] == "profiles"){
+    ?>
+    
+      <script> loadFile('profiles/check_profiles.php'); </script>
+      
+    <?php
+    } else if ($_SESSION['admin_section'] == 'caccounts'){
+    ?>
+    
+      <script> loadFile('accounts/customer_acc.php'); </script>
+      
+    <?php
+    } else if ($_SESSION['admin_section'] == 'invoices'){
+    ?>
+    
+      <script> loadFile('invoices/check_invoices.php'); </script>
+      
+    <?php
+    } else {
+    ?>
+    
+      <script> loadFile('orders/check_orders.php'); </script>
+      
+    <?php
+    }
+    ?>
+  </div>
 
 </div>
 <?php
