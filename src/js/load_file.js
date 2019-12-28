d135bac0d258c1be82cf5aa77268b531607de103
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-28 01:52:48 -0500

    Create load_file.js
    -function to asynchronously load certain pages into the user control panel

---
+++ src/js/load_file.js
@@ -0,0 +1,8 @@
+/*****************************************************
+ *  function to use AJAX to load PHP for the section *
+ *****************************************************/
+ 
+ 
+function loadFile(url){
+  $(".content_window").load(url);
+}
