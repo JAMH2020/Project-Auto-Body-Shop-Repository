2d81f2af6fe60d2029f8bd65f9a4ed078d2200fe
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2019-12-29 18:51:04 -0500

    Reupdated function to get the worker id

--- database/select/aselect_waccounts.php
+++ database/select/aselect_waccounts.php
@@ -2,50 +2,120 @@
 /*********************************************
 ** shows all the worker accounts available  **
 **********************************************/
-//include file to connect to the database
-include_once '../../database/connectdb.php';
-//include file to check errors in sql statements
-include_once '../../database/error_check.php';
-//prepare and bind sql statement
-$stmt_a_waccounts = $conn->prepare("SELECT * FROM Worker_Accounts");
-//execute the statement
-$stmt_a_waccounts->execute();
-//store the result
-$stmt_a_waccounts->store_result();
-//bind the results
-$stmt_a_waccounts->bind_result($worker_idRow, $worker_firstnameRow, $worker_lastnameRow, $worker_passwordRow, $worker_emailRow);
-//print out the accounts that are available
-if ($stmt_a_waccounts->num_rows > 0){
-  //prints out a table
-  echo "<table class='table'>";
-    echo "<tr>";
+
+function list_waccounts(){
+  //include file to connect to the database
+  include_once '../../database/connectdb.php';
+
+  //include file to check errors in sql statements
+  include_once '../../database/error_check.php';
+
+
+  //prepare and bind sql statement
+  $stmt_a_waccounts = $conn->prepare("SELECT * FROM Worker_Accounts");
+
+  //execute the statement
+  $stmt_a_waccounts->execute();
+
+  //store the result
+  $stmt_a_waccounts->store_result();
+
+  //bind the results
+  $stmt_a_waccounts->bind_result($worker_idRow, $worker_firstnameRow, $worker_lastnameRow, $worker_passwordRow, $worker_emailRow);
+
+
+  //print out the accounts that are available
+  if ($stmt_a_waccounts->num_rows > 0){
+
+    //prints out a table
+    echo "<table class='table'>";
+      echo "<tr>";
       
-      echo "<th>Worker Id</th>";
-      echo "<th>Name</th>";
-      echo "<th>Password</th>";
-      echo "<th>Email</th>";
+        echo "<th>Worker Id</th>";
+        echo "<th>Name</th>";
+        echo "<th>Password</th>";
+        echo "<th>Email</th>";
+        echo "<th></th>";
       
-    echo "</tr>";
-  while($stmt_a_waccounts->fetch()){
-    echo "<tr>";
-      echo "<td> <input type='radio' name='order_id' value=" . $worker_idRow .">" . $worker_idRow . "</td>";
-      echo "<td>" . $worker_firstnameRow . " " . $worker_lastnameRow . "</td>";
-      echo "<td>" . $worker_passwordRow . "</td>";
-      echo "<td>" . $worker_emailRow . "</td>";
+      echo "</tr>";
+
+    while($stmt_a_waccounts->fetch()){
+      echo "<tr>";
+        echo "<td>" . $worker_idRow . "</td>";
+        echo "<td>" . $worker_firstnameRow . " " . $worker_lastnameRow . "</td>";
+        echo "<td>" . $worker_passwordRow . "</td>";
+        echo "<td>" . $worker_emailRow . "</td>";
 ?>
 
-            <td>
-              <a href='#' onclick='findCAccountRow("<?php echo $worker_idRow?>", "../../database/select/find_row/find_row_waccounts.php", "change/change_account.php"); return false;'>Edit</a>
-            </td>
+              <td>
+                <a href='#' onclick='findCAccountRow("<?php echo $worker_idRow?>", "../../database/select/find_row/find_row_waccounts.php", "accounts/change/change_account.php"); return false;'>Edit</a>
+              </td>
 
 <?php     
-    echo "</tr>";
+      echo "</tr>";
+    }
+  
+    echo "</table>";
+  
+  } else {
+    echo "<h3 class='conclusion'>" . "There are no accounts available" . "</h3>";
+  }
+
+
+  //close the statement
+  $stmt_a_waccounts->close();
+}
+
+
+
+
+
+function get_waccounts_id($conn){
+  //include file to connect to the database
+  include_once '../../database/connectdb.php';
+
+  //include file to check errors in sql statements
+  include_once '../../database/error_check.php';
+  
+  
+  
+  //prepare and bind sql statement
+  $stmt_a_waccounts = $conn->prepare("SELECT Worker_Id FROM Worker_Accounts WHERE Email = ?");
+  $stmt_a_waccounts->bind_param("s", $worker_email);
+  
+  //worker email
+  $worker_email = $_SESSION['worker_email'];
+  
+
+  //execute the statement
+  $stmt_a_waccounts->execute();
+
+  //store the result
+  $stmt_a_waccounts->store_result();
+
+  //bind the results
+  $stmt_a_waccounts->bind_result($worker_idRow);
+  
+  
+  //print out the accounts that are available
+  if ($stmt_a_waccounts->num_rows > 0){
+  
+    while($stmt_a_waccounts->fetch()){
+  
+      //save the session for the changed id
+      $worker_id = $worker_idRow;
+    }
+    
+  } else {
+    echo "<h1>NO EMAIL FOUND</h1>";
   }
   
-  echo "</table>";
   
-} else {
-  echo "<h3 class='conclusion' >" . "There Are No Accounts Available" . "</h3>";
+  //close the statement
+  $stmt_a_waccounts->close();
+  
+  
+  //return value
+  return $worker_id;
 }
-//close the statement
-$stmt_a_waccounts->close();
+?>
