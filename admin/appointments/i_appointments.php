cdf16c8a9e9c1e34448382b105c71921c12e4707
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2020-01-05 08:54:40 -0500

    Added new section for incoming appointments

---
+++ admin/appointments/i_appointments.php
@@ -0,0 +1,29 @@
+<?php
+//start the session if it has not been started yet
+if (session_start() === null){
+  session_start();
+}
+
+
+$_SESSION['admin_section'] = "iappointments";
+
+
+//session to identify if the user is editting a form
+$_SESSION['editForm'] = false;
+?>
+
+
+  <!--the admin control panel section where they can check the customer profiles in the system-->
+  <!--stylesheet for the invoices table-->
+  <link rel="stylesheet" type="text/css" href="../../database/select/css/cselect_pappointments.css">
+  
+
+
+  <?php
+  //include the file that will print out all the orders
+  include "../../database/select/aselect_appointments.php";
+  
+  incoming_appointment($conn)
+  ?>
+ 
+  <div id="rowText"></div>
