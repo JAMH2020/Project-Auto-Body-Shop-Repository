77f4d3d47fdbcca0806b79983e0dd07d6178c04b
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2020-01-31 05:50:20 -0500

    Update open_tab.js

--- src/js/open_tab.js
+++ src/js/open_tab.js
@@ -3,20 +3,40 @@
  *******************************************************/
 
 
-function openTab(clickedTab){
+function openTab(clickedTab, selectClass){
   $(document).ready(function(){
-    //close all tabs
-    $(".a_option_link").removeClass("opened_tab");
-    $(".a_option_link").removeClass("center_opened_tab");
-    $(".a_option_link").addClass("closed_tab");
   
-    //open the clicked tab
-    $("#" + clickedTab).removeClass("closed_tab");
-    $("#" + clickedTab).addClass("opened_tab");
+    //if the tabs are the first layer tabs
+    if (selectClass == "a_option_link"){
+      //close all tabs
+      $("." + selectClass).removeClass("opened_tab");
+      $("." + selectClass).removeClass("center_opened_tab");
+      $("." + selectClass).addClass("closed_tab");
+  
+      //open the clicked tab
+      $("#" + clickedTab).removeClass("closed_tab");
+      $("#" + clickedTab).addClass("opened_tab");
     
-    //add the left border to the tab if the tab is not the left most tab
-    if (clickedTab != "orders" && clickedTab != "ogorders"){
-      $("#" + clickedTab).addClass("center_opened_tab");
+      //add the left border to the tab if the tab is not the left most tab
+      if (clickedTab != "orders" && clickedTab != "appointments"){
+        $("#" + clickedTab).addClass("center_opened_tab");
+      }
+      
+    //if the tabs are the second layered tabs
+    } else {
+      //close all tabs
+      $("." + selectClass).removeClass("secondary_opened_tab");
+      $("." + selectClass).removeClass("secondary_center_opened_tab");
+      $("." + selectClass).addClass("secondary_closed_tab");
+  
+      //open the clicked tab
+      $("#" + clickedTab).removeClass("secondary_closed_tab");
+      $("#" + clickedTab).addClass("secondary_opened_tab");
+    
+      //add the left border to the tab if the tab is not the left most tab
+      if (clickedTab != "ogorders"){
+        $("#" + clickedTab).addClass("secondary_center_opened_tab");
+      }
     }
   
   });
