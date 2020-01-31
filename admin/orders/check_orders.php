6893883950e9c34b843c3041844f225907c55978
Alex-Au1 <45087631+Alex-Au1@users.noreply.github.com>
2020-01-31 05:15:05 -0500

    Update check_orders.php

--- admin/orders/check_orders.php
+++ admin/orders/check_orders.php
@@ -27,13 +27,15 @@ $_SESSION['admin_section'] = "orders";
     
     //if none of the rows are checked
     if (empty($_POST['order_idArr'])){
-      echo "<h3>Please select an order</h3>";
+      $_SESSION['error_notification'] = "Please Select An Order to Delete";
     } else {
     
       //delete each selected row
       foreach($_POST['order_idArr'] as $key => $value){
         delete_ordersRow($value);
       }
+      
+      $_SESSION['order_done'] = "delete";
     }
   }
   
@@ -50,16 +52,23 @@ $_SESSION['admin_section'] = "orders";
   
   <!--stylesheet for the orders table-->
   <link rel="stylesheet" type="text/css" href="../database/select/css/aselect_orders.css">
-
-  <a href="../worker_cpanel/orders/intake_repair_form.php" class='order'>Create Order</a>
+  
+  <h2 class="section_title">ORDERS</h2>
+  
+  <div class="create_link">
+    <a href="../worker_cpanel/orders/intake_repair_form.php" class='create'>Create Order</a>
+    <div class="create_underline"></div>
+  </div>
+  
   <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" autocomplete = "off">
 
     <?php
      //include the file that will print out all the orders
      include "../../database/select/aselect_orders.php";
     ?>
-
-    <input type="submit" name="delete_order" value="Delete Order">
+    <div class="delete_align">
+      <input type="submit" class="delete" name="delete_order" value="Delete Order">
+    </div>
   </form>
   <div id="rowText"></div>
   
@@ -69,11 +78,10 @@ $_SESSION['admin_section'] = "orders";
   
     $_SESSION['admin_section'] = "orders";
   ?>
-  
+
   <script>redirect_page('../admin_cpanel.php'); </script>
-  
+
   <?php
-  }
+  } 
   ?>
-  
   
